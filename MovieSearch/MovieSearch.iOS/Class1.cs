d018db409825c07cef6d1612f44b00e852a76640
Haukur <haukur91@gmail.com>
2016-11-28 14:11:19 +0000

    UI setup

--- MovieSearch/MovieSearch.iOS/Class1.cs
+++ MovieSearch/MovieSearch.iOS/Class1.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-using MonoTouch.Foundation;
-using MonoTouch.UIKit;
 
 namespace MovieSearch.iOS
 {
     class Class1
     {
+
     }
 }
--- MovieSearch/MovieSearch.iOS/ViewController.cs
+++ MovieSearch/MovieSearch.iOS/ViewController.cs
@@ -2,27 +2,72 @@
 using DM.MovieApi;
 
 using UIKit;
+using CoreGraphics;
 
 namespace MovieSearch.iOS
 {
 	public partial class ViewController : UIViewController
 	{
-		int count = 1;
+        private const int HorizontalMargin = 20;
 
-		public ViewController (IntPtr handle) : base (handle)
+        private const int StartY = 80;
+
+        private const int StepY = 50;
+
+        private int _yCoord;
+
+        public ViewController (IntPtr handle) : base (handle)
 		{
 		}
 
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
-			// Perform any additional setup after loading the view, typically from a nib.
-			Button.AccessibilityIdentifier = "myButton";
-			Button.TouchUpInside += delegate {
-				var title = string.Format ("{0} clicks!", count++);
-				Button.SetTitle (title, UIControlState.Normal);
-			};
             MovieDbFactory.RegisterSettings(new DBSettings());
+
+
+            this.View.BackgroundColor = UIColor.White;
+
+            this._yCoord = StartY;
+
+            var prompt = new UILabel()
+            {
+                Frame = new CGRect(HorizontalMargin, this._yCoord, this.View.Bounds.Width, 50),
+                Text = "Enter words in a movie title: "
+            };
+            this._yCoord += StepY;
+
+            var nameField = new UITextField()
+            {
+                Frame = new CGRect(HorizontalMargin, this._yCoord, this.View.Bounds.Width - 2 * HorizontalMargin, 50),
+                BorderStyle = UITextBorderStyle.RoundedRect,
+                Placeholder = "Title"
+            };
+            this._yCoord += StepY;
+
+            var greetingButton = UIButton.FromType(UIButtonType.RoundedRect);
+            greetingButton.Frame = new CGRect(HorizontalMargin, this._yCoord, this.View.Bounds.Width - 2 * HorizontalMargin, 50);
+            greetingButton.SetTitle("Get movie", UIControlState.Normal);
+            this._yCoord += StepY;
+
+            var greetingLabel = new UILabel()
+            {
+                Frame = new CGRect(HorizontalMargin, this._yCoord, this.View.Bounds.Width, 50)
+            };
+            this._yCoord += StepY;
+
+            greetingButton.TouchUpInside += (sender, args) =>
+            {
+                nameField.ResignFirstResponder();
+                //greetingLabel.Text = "Hello " + nameField.Text;
+            };
+
+            this.View.AddSubview(prompt);
+            this.View.AddSubview(nameField);
+            this.View.AddSubview(greetingButton);
+            this.View.AddSubview(greetingLabel);
+
+
 		}
 
 		public override void DidReceiveMemoryWarning ()
