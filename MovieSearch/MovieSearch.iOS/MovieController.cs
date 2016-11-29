ecd46e5cc97ceb1b141569e10aad0e8a83fa3c37
Erla Hardardottir <erlah@mac.com>
2016-11-29 13:11:40 +0000

    Unicorns and rainbows. HaukLa Out

--- MovieSearch/MovieSearch.iOS/MovieController.cs
+++ MovieSearch/MovieSearch.iOS/MovieController.cs
@@ -8,102 +8,113 @@ using DM.MovieApi.ApiResponse;
 
 namespace MovieSearch.iOS
 {
-    public class MovieController : UIViewController
-    {
-        private const int HorizontalMargin = 20;
+	public class MovieController : UIViewController
+	{
+		private const int HorizontalMargin = 20;
 
-        private const int StartY = 80;
+		private const int StartY = 80;
 
-        private const int StepY = 50;
+		private const int StepY = 50;
 
-        private int _yCoord;
+		private int _yCoord;
 
-        private Movies _movies;
-        public MovieController()
-        {
-            this._movies = new Movies();
-        }
+		private Movies _movies;
+		public MovieController()
+		{
+			this._movies = new Movies();
+		}
 
-        public override void ViewDidLoad()
-        {
-            base.ViewDidLoad();
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
 
-            this.Title = "TITLE";
+			this.Title = "TITLE";
 
-            MovieDbFactory.RegisterSettings(new DBSettings());
-            var movieApi = MovieDbFactory.Create<IApiMovieRequest>().Value;
+			MovieDbFactory.RegisterSettings(new DBSettings());
+			var movieApi = MovieDbFactory.Create<IApiMovieRequest>().Value;
 
-            this.View.BackgroundColor = UIColor.White;
+			this.View.BackgroundColor = UIColor.White;
 
-            this._yCoord = StartY;
+			this._yCoord = StartY;
 
-            var prompt = CreatePrompt();
+			var prompt = CreatePrompt();
 
-            this._yCoord += StepY;
+			this._yCoord += StepY;
 
-            var movieField = CreateMovieField();
+			var movieField = CreateMovieField();
 
-            this._yCoord += StepY;
+			this._yCoord += StepY;
 
-            var searchButton = CreateButton("Get movie");
-            this._yCoord += StepY;
+			var searchButton = CreateButton("Get movie");
+			this._yCoord += StepY;
 
-            this._yCoord += StepY;
-
-            searchButton.TouchUpInside += async (sender, args) =>
-            {
+			searchButton.TouchUpInside += async (sender, args) =>
+			{
 				searchButton.Enabled = false;
+
+				//loading
+				var loading = CreateLoadingSpinner();
+				this.View.AddSubview(loading);
+				loading.StartAnimating();
+
 				movieField.ResignFirstResponder();
-                ApiSearchResponse<MovieInfo> response = await movieApi.SearchByTitleAsync(movieField.Text);
-                var loading = new UIActivityIndicatorView();
-                this.View.AddSubview(loading);
+				ApiSearchResponse<MovieInfo> response = await movieApi.SearchByTitleAsync(movieField.Text);
 
-                _movies.MovieList.Clear();
+				_movies.MovieList.Clear();
 
-                foreach (var r in response.Results)
-                {
-                    _movies.MovieList.Add(r.Title);
-                }
+				foreach (var r in response.Results)
+				{
+					_movies.MovieList.Add(r.Title);
+				}
 
 				this.NavigationController.PushViewController(new MovieListController(this._movies.MovieList), true);
 
 				searchButton.Enabled = true;
-            };
+				loading.StopAnimating();
+			};
 
-            this.View.AddSubview(prompt);
-            this.View.AddSubview(movieField);
-            this.View.AddSubview(searchButton);
-            
-        }
+			this.View.AddSubview(prompt);
+			this.View.AddSubview(movieField);
+			this.View.AddSubview(searchButton);
 
-        private UIButton CreateButton(string title)
-        {
-            var navButton = UIButton.FromType(UIButtonType.RoundedRect);
-            navButton.Frame = new CGRect(HorizontalMargin, this._yCoord, this.View.Bounds.Width - 2 * HorizontalMargin, 50);
-            navButton.SetTitle(title, UIControlState.Normal);
-            return navButton;
-        }
+		}
 
-        private UITextField CreateMovieField()
-        {
-            var movieField = new UITextField()
-            {
-                Frame = new CGRect(HorizontalMargin, this._yCoord, this.View.Bounds.Width - 2*HorizontalMargin, 50),
-                BorderStyle = UITextBorderStyle.RoundedRect,
-                Placeholder = "Title"
-            };
-            return movieField;
-        }
+		private UIButton CreateButton(string title)
+		{
+			var navButton = UIButton.FromType(UIButtonType.RoundedRect);
+			navButton.Frame = new CGRect(HorizontalMargin, this._yCoord, this.View.Bounds.Width - 2 * HorizontalMargin, 50);
+			navButton.SetTitle(title, UIControlState.Normal);
+			return navButton;
+		}
 
-        private UILabel CreatePrompt()
-        {
-            var prompt = new UILabel()
-            {
-                Frame = new CGRect(HorizontalMargin, this._yCoord, this.View.Bounds.Width, 50),
-                Text = "Enter words in a movie title: "
-            };
-            return prompt;
-        }
+		private UITextField CreateMovieField()
+		{
+			var movieField = new UITextField()
+			{
+				Frame = new CGRect(HorizontalMargin, this._yCoord, this.View.Bounds.Width - 2 * HorizontalMargin, 50),
+				BorderStyle = UITextBorderStyle.RoundedRect,
+				Placeholder = "Title"
+			};
+			return movieField;
+		}
+
+		private UILabel CreatePrompt()
+		{
+			var prompt = new UILabel()
+			{
+				Frame = new CGRect(HorizontalMargin, this._yCoord, this.View.Bounds.Width, 50),
+				Text = "Enter words in a movie title: "
+			};
+			return prompt;
+		}
+
+		private UIActivityIndicatorView CreateLoadingSpinner()
+		{
+			UIActivityIndicatorView loading = new UIActivityIndicatorView(UIActivityIndicatorViewStyle.Gray);
+			loading.Frame = new CGRect(HorizontalMargin, this._yCoord, this.View.Bounds.Width - 2 * HorizontalMargin, 50);
+			loading.HidesWhenStopped = true;
+			return loading;
+		}
     }
 }
 
