3cff605debda092f7d1f576395be355eaa526782
Erla Hardardottir <erlah@mac.com>
2016-11-30 12:42:42 +0000

    changed genre and cast arrays to lists of strings

--- MovieSearch/MovieSearch/Model/Movie.cs
+++ MovieSearch/MovieSearch/Model/Movie.cs
@@ -1,18 +1,25 @@
-﻿namespace MovieSearch.Model
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieSearch.Model
 {
-    public class Movie
-    {
-        public Movie() { }
-        public Movie(string title /*TODO: AND MOAR*/)
-        {
-            this.Title = title;
-        }
-        public string Title { get; set; }
+	public class Movie
+	{
+		public Movie() { }
+		public Movie(string title /*TODO: AND MOAR*/)
+		{
+			this.Title = title;
+		}
+		public string Title { get; set; }
 		public string Year { get; set; }
-        public string Genre { get; set; }
+		public List<string> Genre { get; set; }
 		public string Overview { get; set; }
 		public string Poster { get; set; }
-		public string Cast { get; set; }
+		public List<string> Cast { get; set; }
         //TODO: MOAR INFO
     }
 }
