d54db8b6e40c4b8995530e7ec6823020ebf72434
Erla Hardardottir <erlah@mac.com>
2016-12-06 11:38:42 +0000

    new stuff in movieListActivity

--- MovieSearch/MovieSearch.Droid/MovieListActivity.cs
+++ MovieSearch/MovieSearch.Droid/MovieListActivity.cs
@@ -12,14 +12,19 @@ using Android.Widget;
 
 namespace MovieSearch.Droid
 {
-    [Activity(Label = "MovieListActivity")]
-    public class MovieListActivity : Activity
+    [Activity(Theme = "@style/MyTheme", Label = "Movie list")]
+	//ListActivity is similar to TableViewController in ios
+    public class MovieListActivity : ListActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
 
-            // Create your application here
+			//ask the intent object to extract the list that the previous activity sentt
+			var movieList = this.Intent.Extras.GetStringArrayList("titleList") ?? new string[0];
+
+			//A list activity needs an adapter (data source))
+			this.ListAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, movieList); 
         }
     }
 }
