636e0451cc54f17df4418a5a49cddbfa43c3ad23
Erla <erla1011@gmail.com>
2016-12-07 11:04:10 +0000

    toolbar.xml file

--- MovieSearch/MovieSearch.Droid/Resources/Resource.designer.cs
+++ MovieSearch/MovieSearch.Droid/Resources/Resource.designer.cs
@@ -63,32 +63,35 @@ namespace MovieSearch.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060007
-			public const int cast = 2131099655;
-			
-			// aapt resource value: 0x7f060005
-			public const int listItem = 2131099653;
-			
-			// aapt resource value: 0x7f060001
-			public const int movieEditText = 2131099649;
-			
-			// aapt resource value: 0x7f060000
-			public const int movieTextView = 2131099648;
-			
 			// aapt resource value: 0x7f060008
-			public const int poster = 2131099656;
-			
-			// aapt resource value: 0x7f060003
-			public const int progressBar1 = 2131099651;
-			
-			// aapt resource value: 0x7f060004
-			public const int resultTextView = 2131099652;
-			
-			// aapt resource value: 0x7f060002
-			public const int searchButton = 2131099650;
+			public const int cast = 2131099656;
 			
 			// aapt resource value: 0x7f060006
-			public const int title = 2131099654;
+			public const int listItem = 2131099654;
+			
+			// aapt resource value: 0x7f060002
+			public const int movieEditText = 2131099650;
+			
+			// aapt resource value: 0x7f060001
+			public const int movieTextView = 2131099649;
+			
+			// aapt resource value: 0x7f060009
+			public const int poster = 2131099657;
+			
+			// aapt resource value: 0x7f060004
+			public const int progressBar1 = 2131099652;
+			
+			// aapt resource value: 0x7f060005
+			public const int resultTextView = 2131099653;
+			
+			// aapt resource value: 0x7f060003
+			public const int searchButton = 2131099651;
+			
+			// aapt resource value: 0x7f060007
+			public const int title = 2131099655;
+			
+			// aapt resource value: 0x7f060000
+			public const int toolbar = 2131099648;
 			
 			static Id()
 			{
@@ -107,7 +110,13 @@ namespace MovieSearch.Droid
 			public const int Main = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int MovieListItem = 2130903041;
+			public const int MovieInput = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int MovieListItem = 2130903042;
+			
+			// aapt resource value: 0x7f030003
+			public const int toolbar = 2130903043;
 			
 			static Layout()
 			{
